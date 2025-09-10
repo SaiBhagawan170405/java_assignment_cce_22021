faeac341e555a4b47c61654c7a494c9ad9ab71d7
SaiBhagawan170405 <118368671+SaiBhagawan170405@users.noreply.github.com>
2025-09-10 19:03:06 +0530

    Add files via upload

---
+++ assignment -1/DataTypesDemo.java
@@ -0,0 +1,23 @@
+public class DataTypesDemo {
+    public static void main(String[] args) {
+        byte b = 127;
+        short s = 32000;
+        int i = 100000;
+        long l = 1000000000L;
+
+        float f = 5.75f;
+        double d = 19.99;
+
+        char c = 'A';
+        boolean bool = true;
+
+        System.out.println("Byte: " + b);
+        System.out.println("Short: " + s);
+        System.out.println("Int: " + i);
+        System.out.println("Long: " + l);
+        System.out.println("Float: " + f);
+        System.out.println("Double: " + d);
+        System.out.println("Char: " + c);
+        System.out.println("Boolean: " + bool);
+    }
+}
---
+++ assignment -1/Dataoperations.java
@@ -0,0 +1,17 @@
+public class Dataoperations {
+    public static void main(String[] args) {
+        // Arithmetic
+        int a = 10, b = 5;
+        System.out.println("Add: " + (a + b));
+        System.out.println("Subtract: " + (a - b));
+        System.out.println("Multiply: " + (a * b));
+        System.out.println("Divide: " + (a / b));
+        System.out.println("Modulus: " + (a % b));
+
+        // Logical
+        boolean x = true, y = false;
+        System.out.println("AND: " + (x && y));
+        System.out.println("OR: " + (x || y));
+        System.out.println("NOT x: " + (!x));
+    }
+}
